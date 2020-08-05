18016c27adf56c7a75072ad66d367e6f133d18e8
ywszm <aichengwan1@163.com>
2020-08-06 00:05:08 +0800

    修复点击取消提示问题

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -5,7 +5,7 @@ import Component from 'vue-class-component';
 export class TagHelper extends Vue {
   createTag() {
     const name = window.prompt('请输入标签名');
-    if (!name) { return window.alert('标签名不能为空'); }
+    if (!name) { return  }
     this.$store.commit('createTag', name);
   }
 }
