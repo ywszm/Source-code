bd153468828245f93968976d34708fae97fbec47
ywszm <aichengwan1@163.com>
2020-08-06 00:44:25 +0800

    添加源代码库

---
+++ src/components/Money/FormItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label class="formItem">
+      <span class="name">{{this.fieldName}}</span>
+      <input type="text"
+             :value="value"
+             @input="onValueChanged($event.target.value)"
+             :placeholder="this.placeholder">
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop, Watch} from 'vue-property-decorator';
+
+  @Component
+  export default class FormItem extends Vue {
+    @Prop({default: ''}) readonly value!: string;
+
+    @Prop({required: true}) fieldName!: string;
+    @Prop() placeholder?: string;
+
+    onValueChanged(value: string) {
+      this.$emit('update:value', value);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .formItem {
+    font-size: 14px;
+    padding-left: 16px;
+    display: flex;
+    align-items: center;
+    border-radius: 8px;
+    border: 1px;
+    .name {
+      padding-right: 16px;
+    }
+    input {
+      border: 1px solid #cbc7c7;
+      height: 35px;
+      flex-grow: 1;
+      background: #eae9e9;
+      /*border: none;*/
+      margin-right: 10px;
+      padding-left: 5px;
+      border-radius: 4px;
+    }
+  }
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button @click="createTag">
+        <Icon name="add"/>
+      </button>
+    </div>
+    <ul class="current">
+      <li v-for="tag in tagList" :key="tag.id"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">{{tag.name}}
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+  import {mixins} from 'vue-class-component';
+  import TagHelper from '@/mixins/TagHelper';
+
+  @Component
+  export default class Tags extends mixins(TagHelper) {
+    selectedTags: string[] = [];
+
+    get tagList() {
+      return this.$store.state.tagList;
+    }
+
+    created() {
+      this.$store.commit('fetchTags');
+    }
+
+    toggle(tag: string) {
+      const index = this.selectedTags.indexOf(tag);
+      if (index >= 0) {
+        this.selectedTags.splice(index, 1);
+      } else {
+        this.selectedTags.push(tag);
+      }
+      this.$emit('update:value', this.selectedTags);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tags {
+    background: #ffffff;
+    font-size: 14px;
+    padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
+    > .current {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      > li {
+        $bg: #f5f4f4;
+        background: $bg;
+        $h: 24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 16px;
+        margin: 4px 12px 4px 12px;
+        &.selected {
+          background: #ffe384;
+          color: white;
+        }
+      }
+    }
+    > .new {
+      padding-top: 16px;
+      button {
+        background: transparent;
+        border: none;
+        .icon {
+          width: 30px;
+          height: 30px;
+          background: #ffe384;
+          border-radius: 50%;
+
+
+
+        }
+      }
+    }
+  }
+
+</style>
